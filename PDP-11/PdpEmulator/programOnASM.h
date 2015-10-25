e41fb50b0b4288f04936548324bf4472a60ae3ca
awant <mir0810@mail.ru>
2015-10-25 23:49:45 +0400

    added valid asm code

--- PDP-11/PdpEmulator/programOnASM.h
+++ PDP-11/PdpEmulator/programOnASM.h
@@ -7,13 +7,17 @@ wS - width of Screen
 bW - border Width = (hS - hI) / 2
 bH - border Height = (wS - wI) / 2
 
-ѕусть будет квадратна€ картинка и квадратный дисплей
-i = узнаем, когда напишем программу. хот€ можно условитьс€ вз€ть 1/2 от пам€ти
+ѕусть будет квадратна€ картинка и пр€моуг дисплей
+i = 0110000
 v = 040000
-hI = wI = 
-hS = wS = 
-bW = bH = 
-R5 for operation
+file = 0100000
+hI = 128 = 0200
+wI = 128 = 0200
+hS = 256 = 0400
+wS = 512 = 01000
+bH = 64
+bW = 192
+// R5 for operation
 
 // Start program
 MOV #0, R0 // R0 - строки в пиксел€х
@@ -22,28 +26,24 @@ MOV #v, R2 // R2 - address in videobuffer
 MOV #i, R3 // R3 - address of image
 MOV #v, R4
 
-// —осчитаем сколько требуетс€ байтов сверху записать - upBytes, значит вордов - upWords
+// First Version
+// Start
 MOV #0, R0
-@0:
-CMP R0, upWords
+@0:CMP R0, upWords
 BEQ @1
 MOV #0, (R2)+
 ADD #020, R0
 BR @0
-@1:
-MOV #0, R0
-@7:
-MOV #0, R1
+@1:MOV #0, R0
+@7:MOV #0, R1
 CMP R0, hI
 BE @2
-@4:
-CMP R1, bW
+@4:CMP R1, bW
 BGE @3
 MOV #0, (R2)+
 ADD #020, R1
 BE @4
-@3:
-MOV #0, R5
+@3:MOV #0, R5
 ADD #wS, R5
 SUB #bW, R5
 CMP R1, R5
@@ -51,25 +51,102 @@ BGE @5
 MOV (R3)+, (R2)+
 ADD #020, R1
 BE @3
-@5:
-MOV #0, R5
+@5:MOV #0, R5
 ADD #wS, R5
 CMP R1, R5
 BGE @6
 MOV (R3)+ , (R2)+
 ADD #020, R1
 BE @5
-@6:
-ADD #01, R0
+@6:ADD #01, R0
 BE @7
-@2:
-// —осчитаем сколько требуетс€ байтов снизу записать - bottomBytes, значит вордов - bottomWords
-MOV #0, R0
-@8:
-CMP R0, bottomWords
+@2:MOV #0, R0
+@8:CMP R0, bottomWords
 BEQ @9
 MOV #0, (R2)+
 ADD #020, R0
 BR @8
-@9:
-// That's all
+@9:HALT
+
+// Second Version
+// Start
+MOV #0, R0
+CMP R0, upWords
+BEQ 9
+MOV #0, (R2)+
+ADD #020, R0
+BR -12
+MOV #0, R0
+MOV #0, R1
+CMP R0, hI
+BE 60
+CMP R1, bW 
+BGE 9
+MOV #0, (R2)+
+ADD #020, R1
+BE -12
+MOV #0, R5
+ADD #wS, R5
+SUB #bW, R5
+CMP R1, R5
+BGE 9
+MOV (R3)+ , (R2)+
+ADD #020, R1
+BE -21
+MOV #0, R5
+ADD #wS, R5
+CMP R1, R5
+BGE 9
+MOV (R3)+ , (R2)+
+ADD #020, R1
+BE -18
+ADD #01, R0
+BE -66
+MOV #0, R0
+CMP R0, bottomWords
+BEQ 9
+MOV #0, (R2)+
+ADD #020, R0
+BR -12
+HALT
+
+// Third Version
+MOV #0, R0
+CMP R0, upWords
+BEQ 9
+MOV #0, (R2)+
+ADD #020, R0
+BR - 12
+MOV #0, R0
+MOV #0, R1
+CMP R0, hI
+BE 60
+CMP R1, bW
+BGE 9
+MOV #0, (R2)+
+ADD #020, R1
+BE - 12
+MOV #0, R5
+ADD #wS, R5
+SUB #bW, R5
+CMP R1, R5
+BGE 9
+MOV(R3) + , (R2)+
+ADD #020, R1
+BE - 21
+MOV #0, R5
+ADD #wS, R5
+CMP R1, R5
+BGE 9
+MOV(R3) + , (R2)+
+ADD #020, R1
+BE - 18
+ADD #01, R0
+BE - 66
+MOV #0, R0
+CMP R0, bottomWords
+BEQ 9
+MOV #0, (R2)+
+ADD #020, R0
+BR - 12
+HALT
