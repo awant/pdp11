ea89fc3f00c3ce28c47cb3e986ea0d907f04c58b
mvilkin <vil_ms@mail.ru>
2015-11-11 01:20:17 +0300

    Sending registers and flags from emu to gui.

--- PDP-11/PdpEmulator/PdpEmulatorInterface.cpp
+++ PDP-11/PdpEmulator/PdpEmulatorInterface.cpp
@@ -4,28 +4,12 @@
 #include "PdpEmulatorInterface.h"
 #include "PdpEmulator.h"
 
-int Sum(int a, int b)
-{
-	// my Emulator
-	return a + b;
-}
-
-int Sub(int a, int b)
-{
-	return a - b;
-}
-
-int Check()
-{
-	return PdpEmulator::I().Check(010102);
-}
-
 void GetVideoBuffer(char * buffer)
 {
 	offset_t startOfVideoBuffer = 040000;
 	offset_t sizeOfVideoBuffer = 512 * 256 / 8;
 	auto emu = PdpEmulator::IPtr();
-	int num = 9;
+	int num = 100;
 	while (num-- > 0) {
 		emu->PerformCurrentInstruction();
 	}
@@ -38,4 +22,17 @@ void GetCurrentInstruction(char * buffer)
 	auto string = emu->GetCurrentInstruction();
 	auto cString = string.c_str();
 	memcpy(buffer, cString, string.length() + 1);
+}
+
+void GetRegisters(int * buffer)
+{
+	auto emu = PdpEmulator::IPtr();
+	for (int i = 0; i < 8; i++)
+		buffer[i] = emu->GetRegisterValue(i);
+}
+
+int GetFlags()
+{
+	auto emu = PdpEmulator::IPtr();
+	return emu->GetFlagsByte();
 }
--- PDP-11/PdpEmulator/PdpEmulatorInterface.h
+++ PDP-11/PdpEmulator/PdpEmulatorInterface.h
@@ -12,13 +12,12 @@ extern "C"
 	/*PDP_EMU_API BOOL pdpInitializer();
 
 	PDP_EMU_API char** getAllInstructions();
-	PDP_EMU_API int getIndexOfCurrentInstruction();
-	PDP_EMU_API int* getRegisters();
-	PDP_EMU_API int* getStatusFlags();*/
+	PDP_EMU_API int getIndexOfCurrentInstruction();*/
 
 	PDP_EMU_API void GetVideoBuffer(char * buffer);
 	PDP_EMU_API void GetCurrentInstruction(char * buffer);
-	PDP_EMU_API int Check();
+	PDP_EMU_API void GetRegisters(int * buffer);
+	PDP_EMU_API int GetFlags();
 }
 
 #endif // _PDP_EMULATOR_INTERFACE_H_
